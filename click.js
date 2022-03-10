c7c327c7ed07d3c76145136e050da8a72aac162d
cleargoaldigital <cleargoal.ng@gmail.com>
2022-03-10 21:29:55 +0100

    more on classes

--- click.js
+++ click.js
@@ -1,6 +1,6 @@
  class Sokaschools {
    set location(newLocation){
-     if(newLocation.length<5){
+     if(newLocation.length < 5){
        this._location = "Location: Name must be at least 5 characters long."
      } else{
      this._location = newLocation;
@@ -24,9 +24,13 @@
   }
    
    get year(){
-
      return this._year;
    }
+
+   // Add values to existing values
+   changeYear(newvalue){
+    this.year = parseInt(this.year) + parseInt(newvalue);
+  }
 
    constructor(location, year){
     this.location = location;
@@ -36,17 +40,71 @@
  
  let micfis = new Sokaschools('Ire-Akari', 2013);
 
-//  return micfis.location;
- 
  let mariny = new Sokaschools('Soka', 2016);
  
  let fazl = new Sokaschools('Monatan', 1996);
- 
- let newloc = "ade";
- console.log(newloc.length);
  
 console.log('MICFIS was established in the year ' + micfis.year + ' and is located at ' + micfis.location + '.');
 
 console.log('MARINY School was established in the year ' + mariny.year + ' and is located at ' + mariny.location + '.');
 
 console.log('FAZL-OMAR College was established in the year ' + fazl.year + ' and is located at ' + fazl.location + '.');
+
+// console.log(mariny.changeYear(12));
+
+
+class Person{
+
+  set fname(first){
+    this._fname = first;
+  }
+
+  get fname(){
+    return this._fname;
+  }
+
+  set lname(last){
+    this._lname = last;
+  }
+
+  get lname(){
+    return this._lname;
+  }
+
+  fullName(greet=false){
+    let allName = this.makeName(greet);
+    return allName;
+  }
+  constructor(fname, lname){
+    this.fname = fname;
+    this.lname = lname;
+  }
+
+  makeName(greet){
+    if(greet === true){
+      return this.addGreet() + ' '+ this._fname + ' '+ this._lname;
+    }
+      return this._fname + ' '+ this._lname;
+    }
+  
+  addGreet(){
+    return "Hello";
+  }
+
+}
+
+let father = new Person("Taofeek", "Ajibade");
+
+// console.log(father.fname + ' ' + father.lname);
+
+let mother = new Person("Hibat", "Ajibade")
+
+// console.log(mother.fname + ' ' + mother.lname);
+
+let son = new Person("Abdulhayy", "Ajibade")
+
+// console.log(son.fname + ' ' + son.lname);
+
+console.log(father.fullName(true));
+console.log(mother.fullName(true));
+console.log(son.fullName(true));
