a48ec5bf9a104a10159a6f015d0870540694f8f3
cleargoaldigital <cleargoal.ng@gmail.com>
2022-03-11 20:17:36 +0100

    added inheritance

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Some experimentation with JavaScript classes.
+
+**In JavaScript, inheritance is achieved with the keyword, extends.**
--- click.js
+++ click.js
@@ -108,3 +108,93 @@ let son = new Person("Abdulhayy", "Ajibade")
 console.log(father.fullName(true));
 console.log(mother.fullName(true));
 console.log(son.fullName(true));
+
+
+class Address{
+  get city(){
+    return this._city;
+  }
+
+  constructor(city, state){
+    this._city = city;
+    this._state = state;
+  }
+}
+
+class Employee{
+  get address(){
+    return this._address
+  }
+
+  constructor(emId, name, address){
+    this._emId = emId;
+    this._name = name;
+    this._address = address;
+  }
+}
+
+class Customer{
+  get address(){
+    return this._address
+  }
+
+  constructor(custId, name, address){
+    this._custId = custId;
+    this._name = name;
+    this._address = address;
+  }
+}
+
+let empAddress = new Address('Modakeke', 'Osun');
+let staff1 = new Employee(1, 'Taofeek', empAddress);
+
+let custAddress = new Address('Kuta', 'West');
+let customer1 = new Customer(1, 'ClearGoal Digital Solutions', custAddress);
+
+console.log(staff1.address.city);
+console.log(customer1.address.city);
+
+// Basics of Javascript Inheritance
+
+class Personal{
+  set names(n){
+    this._names = n;
+  }
+
+  get names(){
+    return this._names;
+  }
+  
+  constructor(n){
+    this._names = n;
+  }
+}
+
+class Employees extends Personal{
+  set title(t){
+    this._title = t;
+  }
+  get title(){
+    return this._title
+  }
+  constructor(n, t){
+    super(); //Invokes super first
+    this._names = n; //Invokes super's inherited setter
+    this._title = t; //set local variable
+  }
+}
+
+let staff2 = new Employees('Akanbi', 'Director'); //Use Employee Constructor
+console.log(staff2.names); //Super's getter
+console.log(staff2.title); //Employee's setter
+
+let staff3 = new Employees();
+staff3.name = 'Adewale';
+staff3.title = 'Developer';
+
+console.log(staff3.name);
+console.log(staff3.title);
+
+
+let human = new Personal('Ajibade');
+console.log(human.names)
